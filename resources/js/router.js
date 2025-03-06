ec516b9a2d671dead4d67066169395bcc291e559
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-03-06 16:50:43 +0530

    update router

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from './components/Home.vue'
+import Home from './components/Home.vue';
+import Login from './components/Login.vue';  
+import Register from './components/Register.vue';  
+import Dashboard from './components/Dashboard.vue';  
+import NotFound from './components/NotFound.vue';
 
 const routes = [
   { path: '/', component: Home, name: 'Home' },
+  { path: '/login', component: Login, name: 'login' },
+  { path: '/register', component: Register, name: 'register' },
+  { path: '/dashboard', component: Dashboard, name: 'dashboard' },
+  { path: '/:pathMatch(.*)*', component: NotFound },
 ];
 
 const router = createRouter({
