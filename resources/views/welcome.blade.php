d978246440a9f334cc47821745cadbcd1bf2f531
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-03-06 15:15:23 +0530

    Update welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <div id="app">
-            <example-component></example-component>
+            <!-- <example-component></example-component> -->
         </div>
     </body>
 </html>
