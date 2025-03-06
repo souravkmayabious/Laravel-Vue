f6a54441ee13a50bf02cab16790f1722572f9cf6
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-03-06 16:52:28 +0530

    update app.vue adding header , footer , navbar

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,30 @@
 <template>
     <div id="app">
+      <Header /> 
+      <Navbar /> 
       <router-view></router-view> 
+      <Footer />
     </div>
 </template>
   
 <script>  
+import Navbar from './components/common/Navbar.vue';
+import Header from './components/common/Header.vue';
+import Footer from './components/common/Footer.vue';
+
   export default {
     name: 'App',
+      components: {
+      Navbar,
+      Header,
+      Footer,
+    },
   };
 </script>
   
 <style scoped>
+#app {
+  text-align: center;
+  font-family: 'Arial', sans-serif;
+}
 </style>
