92b6cff6d0c94ca0f837b0b0445bc1c1ee87e462
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-03-06 16:59:14 +0530

    Create Protected route

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,17 +5,46 @@ import Register from './components/Register.vue';
 import Dashboard from './components/Dashboard.vue';  
 import NotFound from './components/NotFound.vue';
 
+
+// Check if the user is authenticated
+function isAuthenticated() {
+  return localStorage.getItem('auth_token') !== null;
+}
+
 const routes = [
   { path: '/', component: Home, name: 'Home' },
-  { path: '/login', component: Login, name: 'login' },
-  { path: '/register', component: Register, name: 'register' },
-  { path: '/dashboard', component: Dashboard, name: 'dashboard' },
+  { path: '/login', component: Login, name: 'login', meta: { requiresGuest: true } },
+  { path: '/register', component: Register, name: 'register', meta: { requiresGuest: true } },
+  { path: '/dashboard', component: Dashboard, name: 'dashboard', meta: { requiresAuth: true }  },
   { path: '/:pathMatch(.*)*', component: NotFound },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+
+
+// Global middleware (navigation guard) to check for authentication
+router.beforeEach((to, from, next) => {
+  // Check for routes that require authentication
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!isAuthenticated()) {
+      next({ name: 'login' });
+    } else {
+      next(); 
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    // Redirect to dashboard if already authenticated and trying to access login/register
+    if (isAuthenticated()) {
+      next({ name: 'dashboard' });
+    } else {
+      next(); 
+    }
+  } else {
+    next(); 
+  }
 });
 
 export default router;
